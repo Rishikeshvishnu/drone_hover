949c5d19e50abba1548b5c795cccdd34dc9c8825
Rishikeshvishnu <rishikeshvishnusivakumar@gmail.com>
2025-11-14 01:01:00 +0100

    Update image processing node.Tweaks needed

--- build/drone_img_processing/prefix_override/sitecustomize.py
+++
@@ -1,4 +0,0 @@
-import sys
-if sys.prefix == '/usr':
-    sys.real_prefix = sys.prefix
-    sys.prefix = sys.exec_prefix = '/home/rishikesh/Desktop/drone_hover/install/drone_img_processing'
--- src/drone_img_processing/drone_img_processing/drone_img_coord_pub.py
+++ src/drone_img_processing/drone_img_processing/drone_img_coord_pub.py
@@ -2,127 +2,131 @@ import rclpy
 from rclpy.node import Node
 
 from sensor_msgs.msg import Image
+from std_msgs.msg import String
+import json
 
 from cv_bridge import CvBridge
 import cv2
 import numpy as np
-
-from ultralytics import YOLO
 
 
 class DroneImageCoordinates(Node):
     def __init__(self):
         super().__init__('drone_image_coordinates')
         
-        self.drone_coords_pub = self.create_publisher(Image, 'drone_image', 10)
+        self.drone_coords_pub = self.create_publisher(String, 'drone_2D_points', 10)
         
-        timer_period = 1/10 # 10 hertz
-        self.create_timer(timer_period, self.timer_callback)
+        timer_period = 1/10 
+        self.create_timer(timer_period, self._timer_callback)
 
-        self.cap = cv2.VideoCapture(0)
-        if not self.cap.isOpened():
-            self.get_logger().error('Couldnt open webcam. Shutting down')
+        # start vc , use v4l2 for better cam control
+        self.cap1 = cv2.VideoCapture(2, cv2.CAP_V4L2)
+        self.cap2 = cv2.VideoCapture(3, cv2.CAP_V4L2)
+
+        if not self.cap1.isOpened() or not self.cap2.isOpened():
+            self.get_logger().error('Couldnt open one or both webcams. Shutting down')
             return
         
-        self.bridge = CvBridge()
+        # set the pixel identifier format, convert the value into an integer code
+        self.cap1.set(cv2.CAP_PROP_FOURCC, cv2.VideoWriter_fourcc('G','R','B','G'))
+        self.cap2.set(cv2.CAP_PROP_FOURCC, cv2.VideoWriter_fourcc('G','R','B','G'))
 
-        self.model = YOLO('yolo/runs/detect/train3/weights/best.pt')
-        self.model.to('cuda:0')
-        
-    def drone_yolo(self):
+        # set width
+        self.cap1.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
+        self.cap2.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
+
+        # set height
+        self.cap1.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
+        self.cap2.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
+
+        # set fps
+        self.cap1.set(cv2.CAP_PROP_FPS, 50)
+        self.cap2.set(cv2.CAP_PROP_FPS, 50)
+       
+    def _display_cams(self):
         while True:
-            ret,frame = self.cap.read()
-            if not ret:
-                self.get_logger().warning('Failed to read frame from webcam.')
+            ret1, frame1 = self.cap1.read()
+            ret2, frame2 = self.cap2.read()
+
+            if not ret1 or not ret2:
+                self.get_logger().warning('Failed to read frame from one or both webcams.')
                 return
+
+            frame1_rs = cv2.cvtColor(frame1, cv2.COLOR_BGR2GRAY)
+            frame2_rs = cv2.cvtColor(frame2, cv2.COLOR_BGR2GRAY)
+
+            bw1 = cv2.inRange(frame1_rs, 250, 255)
+            bw2 = cv2.inRange(frame2_rs, 250, 255)
+
+            conts1, _ = cv2.findContours(bw1, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+            conts2, _ = cv2.findContours(bw2, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+            frame_with_cnts_1 = frame1.copy()
+            frame_with_cnts_2 = frame2.copy()
+
+            centers1 = self._contour_centers(conts1, frame_with_cnts_1)
+            centers2 = self._contour_centers(conts2, frame_with_cnts_2)
+
+            midpoint1 = self._midpoint(centers1)
+            midpoint2 = self._midpoint(centers2)
             
-            cv_img = frame
-            bb_img = cv_img.copy()
-
-            res = self.model(cv_img, device = 'cuda:0')[0]
-
-            if len(res.boxes.data) == 0:
-                self.get_logger().warning('Failed to read detect drone')
-            else:
-                detection = res.boxes.data.tolist()
-                best_detect = max(detection, key = lambda det: det[4])
-
-                x1,y1,x2,y2,conf,cls = best_detect
-                x1, y1, x2, y2 = map(int, (x1, y1, x2, y2))
-
-                cv2.rectangle(bb_img, (x1, y1), (x2, y2), (0, 255, 0), 2)
-                cx = (x1 + x2)//2
-                cy = (y1 + y2)//2
-                cv2.circle(bb_img, (cx,cy), radius=5, color=(0, 0, 255), thickness=-1)
-
-                #bb.center.x = (x1 + x2)/2
-                #bb.center.y = (y1 + y2)/2
-                
-            cv2.imshow('YOLO live feed', bb_img)
-
-            if cv2.waitKey(1) & 0xFF == ord('q'):
-                break
-
-        cv2.destroyAllWindows()
-
-    # def drone_cp(self):
-    #     while True:
-    #         ret,frame = self.cap.read()
-    #         if not ret:
-    #             self.get_logger().warning('Failed to read frame from webcam.')
-    #             return
-    #         hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-
-    #         # blue color range
-    #         lower_blue = (100, 150, 0)
-    #         upper_blue = (140, 255, 255)
-
-    #         mask = cv2.inRange(hsv, lower_blue, upper_blue)
-    #         kernel = np.ones((5, 5), np.uint8)
-    #         mask = cv2.morphologyEx(mask, cv2.MORPH_OPEN, kernel)
-    #         mask = cv2.morphologyEx(mask, cv2.MORPH_CLOSE, kernel)
-
-    #         cv2.imshow('Mask', mask)
-
-    #         contours, _ = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-
-    #         led_centers = []
-    #         for contour in contours:
-    #             M = cv2.moments(contour)
-    #             if M["m00"] != 0:
-    #                 cx = int(M["m10"] / M["m00"])
-    #                 cy = int(M["m01"] / M["m00"])
-    #                 led_centers.append((cx, cy))
+            points = {'cam1':midpoint1, 'cam2':midpoint2}
             
-    #         result = frame.copy()
-
-    #         if len(led_centers) == 2:
-    #             point1 = led_centers[0]
-    #             point2 = led_centers[1]
-    #             midpoint_x = (point1[0] + point2[0]) // 2
-    #             midpoint_y = (point1[1] + point2[1]) // 2
-    #             midpoint = (midpoint_x, midpoint_y)
-    #             cv2.circle(result, midpoint, 5, (0, 0, 255), -1)
-
-    #         else:
-    #             self.get_logger().info('Did not detect exactly two blue LEDs.')
-
             
-    #         cv2.imshow('Result', result)
-    #         if cv2.waitKey(1) & 0xFF == ord('q'):
-    #             break
+            return points
 
-    #     cv2.destroyAllWindows()
+        #     if midpoint1:
+        #         cv2.circle(frame_with_cnts_1, midpoint1, 2, (255, 0, 0), -1)
 
+        #     if midpoint2:
+        #         cv2.circle(frame_with_cnts_2, midpoint2, 2, (255, 0, 0), -1)
+
+
+        #     cv2.imshow('Cam1 feed with m.p', frame_with_cnts_1)
+        #     cv2.imshow('Cam2 feed with m.p', frame_with_cnts_2)
+
+
+        #     if cv2.waitKey(1) & 0xFF == ord('q'):
+        #         break
+
+        # cv2.destroyAllWindows()
+
+    def _contour_centers(self, contours, frame):
+        centers = []
+
+        for cnt in contours:
+            M = cv2.moments(cnt)
+            if M['m00'] == 0:
+                continue
+            cx = int(M['m10'] / M['m00'])
+            cy = int(M['m01'] / M['m00'])
+            b, g, r = frame[cy, cx]
+            if r > max(b, g): 
+                continue
+            centers.append((cx, cy))
+
+        centers.sort(key=lambda c: c[0])
+        return centers
+
+
+    def _midpoint(self, centers):
+
+        if len(centers) < 2:
+            return None
+
+        (x1, y1), (x2, y2) = centers[:2]
         
-    
-    def timer_callback(self):
-        ret,frame = self.cap.read()
-        if ret:
-            msg = self.bridge.cv2_to_imgmsg(frame, encoding = 'bgr8')
-            self.drone_coords_pub.publish(msg)
-        else:
-            self.get_logger().warning('Failed to read frame from webcam.')
+        x = int((x1 + x2) / 2)
+        y = int((y1 + y2) / 2)
+
+        return {'x':x,'y':y}
+
+    def _timer_callback(self):
+        mps = self._display_cams()
+        msg = String()
+        msg.data = json.dumps(mps)
+
+        self.drone_coords_pub.publish(msg)
     
     
 def main():
@@ -130,9 +134,7 @@ def main():
 
     img_test = DroneImageCoordinates()
 
-    drone_cp_test = img_test.drone_yolo()
-
-    # rclpy.spin(img_test)
+    rclpy.spin(img_test)
     img_test.destroy_node()
     rclpy.shutdown()
 
